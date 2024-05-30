3424b8010f34da32a338b971d616b91307a80250
octane night <1462929847@qq.com>
2024-05-30 16:42:45 +0800

    f

---
+++ .utmp/Debug/48506r33/prefab/arm64-v8a/prefab-configure/lib/aarch64-linux-android/cmake/games-frame-pacing/games-frame-pacingConfig.cmake
@@ -0,0 +1,18 @@
+if(NOT TARGET games-frame-pacing::swappy)
+add_library(games-frame-pacing::swappy SHARED IMPORTED)
+set_target_properties(games-frame-pacing::swappy PROPERTIES
+    IMPORTED_LOCATION "C:/Users/octane/.gradle/caches/transforms-4/becec8da3fbf365033ae5d9849f7e2a6/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy/libs/android.arm64-v8a_API31_NDK23_cpp_shared_Release/libswappy.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/octane/.gradle/caches/transforms-4/becec8da3fbf365033ae5d9849f7e2a6/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET games-frame-pacing::swappy_static)
+add_library(games-frame-pacing::swappy_static STATIC IMPORTED)
+set_target_properties(games-frame-pacing::swappy_static PROPERTIES
+    IMPORTED_LOCATION "C:/Users/octane/.gradle/caches/transforms-4/becec8da3fbf365033ae5d9849f7e2a6/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy_static/libs/android.arm64-v8a_API31_NDK23_cpp_shared_Release/libswappy.a"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/octane/.gradle/caches/transforms-4/becec8da3fbf365033ae5d9849f7e2a6/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy_static/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
